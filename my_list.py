649126cc85e982c0008901f789527eaab4329b28
Samuell23 <128612706+Samuell23@users.noreply.github.com>
2024-12-09 01:02:55 +0300

    Update my_list.py

--- my_list.py
+++ my_list.py
@@ -1,6 +1,4 @@
-Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def calculate_discount(price, discount_percent):
+ def calculate_discount(price, discount_percent):
     """
     Calculates the final price after applying a discount.
     If the discount is 20% or higher, applies the discount.
